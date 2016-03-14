4cbbe2bcada2fc54030d8ad01a5dd47df2c8d5d8
Maverick Chardet <mchardet@ens-lyon.fr>
2016-03-14 17:21:16 +0100

    Structures de données mises à jour

--- structures.hpp
+++ structures.hpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <string>
+#include <iostream>
 #include <cassert>
 
 typedef std::string Attribute;
@@ -9,10 +10,28 @@ struct Relation {
 	std::vector<Attribute> attributes;
 };
 
+std::ostream& operator<<(std::ostream& stream, const Relation& rel) {
+	stream << rel.name << '(';
+	bool first{true};
+	for (const auto& attr : rel.attributes) {
+		if (first) {
+			first = false;
+		} else {
+			stream << ',';
+		}
+		stream << attr;
+	}
+	return stream;
+}
+
 using Schema = std::vector<Relation>;
 
-struct Variable;
-struct Constant;
+std::ostream& operator<<(std::ostream& stream, const Schema& sc) {
+	for (const auto& rel : sc) {
+		stream << rel << std::endl;
+	}
+	return stream;
+}
 
 struct Value {
 	typedef std::string variable_t;
@@ -23,11 +42,11 @@ struct Value {
 	Value(constant_t cons) : type{Type::CONSTANT}, cons_{cons} {}
 	
 	const Type type;
-	const variable_t& asVariable() {
+	const variable_t& asVariable() const {
 		assert(type == Type::VARIABLE);
 		return var_;
 	}
-	constant_t asConstant() {
+	constant_t asConstant() const {
 		assert(type == Type::CONSTANT);
 		return cons_;
 	}
@@ -36,16 +55,70 @@ private:
 	constant_t cons_;
 };
 
+std::ostream& operator<<(std::ostream& stream, const Value& val) {
+	if (val.type == Value::Type::CONSTANT) {
+		stream << val.asConstant();
+	} else {
+		stream << '$' << val.asVariable();
+	}
+	return stream;
+}
+
 struct Atom {
 	std::string name;
 	std::vector<Value> args;
 };
 
+std::ostream& operator<<(std::ostream& stream, const Atom& atom) {
+	stream << atom.name << '(';
+	bool first{true};
+	for (const auto& attr : atom.args) {
+		if (first) {
+			first = false;
+		} else {
+			stream << ',';
+		}
+		stream << attr;
+	}
+	return stream;
+}
+
 using Query = std::vector<Atom>;
+
+std::ostream& operator<<(std::ostream& stream, const Query& query) {
+	bool first{true};
+	for (const auto& atom : query) {
+		if (first) {
+			first = false;
+		} else {
+			stream << " ^ ";
+		}
+		stream << atom;
+	}
+	return stream;
+}
 
 struct Tgd {
 	Query from;
 	Query to;
 };
 
+std::ostream& operator<<(std::ostream& stream, const Tgd& tgd) {
+	stream << tgd.from << "  ->  " << tgd.to;
+	return stream;
+}
+
 using Mapping = std::vector<Tgd>;
+
+std::ostream& operator<<(std::ostream& stream, const Mapping& mapping) {
+	for (const auto& tgd : mapping) {
+		stream << tgd << std::endl;
+	}
+	return stream;
+}
+
+struct FileContent {
+	Schema source;
+	Schema target;
+	Mapping mapping;
+};
